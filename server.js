4a5ec77edbe65575e3c4db473e61992501887687
babaphillips <amandavphillips@icloud.com>
2022-08-05 22:15:31 -0400

    Heroku 2nd time is the charm

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 const { notes } = require("./data/notes");
 // Just like any other npm package, we will require Express.js
 const express = require("express");
+
+//When Heroku runs our app, it sets an environment variable called process.env.PORT. We're going to tell our app to use that port, if it has been set, and if not, default to port 3001.
+const PORT = process.env.PORT || 3001;
+
 // Setting up the server only takes two steps: we need to instantiate the server, then tell it to listen for requests. To instantiate the server
 const app = express();
 
 //This function will take in req.query as an argument and filter through the notes accordingly, returning the new filtered array
 function filterByQuery(query, notesArray) {
+  // new var filteredResults will equals notesArray parameter
   let filteredResults = notesArray;
+  // filtering thru titles on notes
   if (query.title) {
     filteredResults = filteredResults.filter(
       (note) => note.title === query.title
@@ -29,6 +35,6 @@ app.get("/notes", (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`API server now on port 3001!`);
 });
