5c001718ce962d08105072724e1cb17c0418ec48
babaphillips <amandavphillips@icloud.com>
2022-08-05 22:52:57 -0400

    server receiving data correctly

--- server.js
+++ server.js
@@ -53,6 +53,16 @@ function createNewNote(body, notesArray) {
   return note;
 }
 
+function validateNote(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if (!note.text || typeof note.text !== "string") {
+    return false;
+  }
+  return true;
+}
+
 app.get("/notes", (req, res) => {
   let results = notes;
   // req.query is multifaceted, often combining multiple parameters
@@ -78,9 +88,14 @@ app.post("/notes", (req, res) => {
   // set id based on what the next index of the array will be
   //Now when we receive new post data to be added to the notes.json file, we'll take the length property of the notes array and set that as the id for the new data. Remember, the length property is always going to be one number ahead of the last index of the array so we can avoid any duplicate values.
   req.body.id = notes.length.toString();
-  //add note to json file and animals array in this function
-  const note = createNewNote(req.body, notes);
-  res.json(note);
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    //add note to json file and animals array in this function
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 app.listen(PORT, () => {
