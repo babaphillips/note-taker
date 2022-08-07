14f05e7398ae4a178e850503bbbe65ad3407285b
babaphillips <amandavphillips@icloud.com>
2022-08-07 09:33:09 -0400

    using crypto npm package for id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
-const { readFromFile, createNewNote, readAndAppend } = require("../lib/notes");
+const {
+  readFromFile,
+  findById,
+  createNewNote,
+  readAndAppend,
+} = require("../lib/notes");
+const crypto = require("crypto");
 
+// getting data from my db.json file passing the data
 router.get("/notes", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 //POST requests differ from GET requests in that they represent the action of a client requesting the server to accept data
 router.post("/notes", (req, res) => {
-  // set id based on what the next index of the array will be
-
+  req.body.id = crypto.randomUUID();
   readAndAppend(req.body, "./db/db.json");
   res.json("added new note!");
 });
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
+
 //The * will act as a wildcard, meaning any route that wasn't previously defined will fall under this request and will receive the homepage as the response.
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html"));
